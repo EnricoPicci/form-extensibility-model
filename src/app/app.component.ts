61e415ada691b92e9c3acd436b0b16e375e500d2
Picci <enrico.piccinin@gmail.com>
2023-05-18 10:37:41 +0200

    tutorial starting point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { QuestionBase } from './question-base';
+import { QuestionService } from './question.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'form-extensibility-model';
+export class AppComponent implements OnInit {
+  questions$!: Observable<QuestionBase<any>[]>;
+
+  constructor(private service: QuestionService) {}
+  ngOnInit(): void {
+    this.questions$ = this.service.getQuestions();
+  }
 }
